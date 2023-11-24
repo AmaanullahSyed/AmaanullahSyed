b6aa08ab7b5cb539dd339f7990ba2b75bbd39f0b
AmaanullahSyed <amaanullah.syed@dcmail.ca>
2023-11-24 17:21:14 -0500

    Update main.js

--- Lab4_Part1/main.js
+++ Lab4_Part1/main.js
@@ -27,13 +27,14 @@ function resultconst (){
 
     if(customName.value !== '') {
         const name = customName.value;
-    
+        newStory = newStory.replaceAll('Bob', name);
       }
     
       if(document.getElementById("uk").checked) {
         const weight = Math.round(300);
         const temperature =  Math.round(94);
-    
+        newStory = newStory.replaceAll('94 fahrenheit', temperature);
+        newStory = newStory.replaceAll('300 pounds', weight);
       }
     
       story.textContent = newStory;
